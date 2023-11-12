3a352356732846f2a6467352737a0acc4be92488
WMD11 <113972186+WMD11@users.noreply.github.com>
2023-11-12 23:42:44 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,36 +1,6 @@
 /* Exercițiul 1
 Codul HTML conține o listă de categorii ul#categories.
 
-<ul id="categories">
-  <li class="item">
-    <h2>Animals</h2>
-    <ul>
-      <li>Cat</li>
-      <li>Hamster</li>
-      <li>Horse</li>
-      <li>Parrot</li>
-    </ul>
-  </li>
-  <li class="item">
-    <h2>Products</h2>
-    <ul>
-      <li>Bread</li>
-      <li>Prasley</li>
-      <li>Cheese</li>
-    </ul>
-  </li>
-  <li class="item">
-    <h2>Technologies</h2>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-      <li>React</li>
-      <li>Node.js</li>
-    </ul>
-  </li>
-</ul>
-
 Scrieți un script care:
 
 Va număra și afișa în consolă numărul total de categorii din ul#categories, adică elementele li.item.
